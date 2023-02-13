ac2e77410abe23f8f069020cb35d085543702c50
edwardsuwirya <edward_suwirya@yahoo.com>
2023-02-13 09:06:08 +0700

    Refactor separate into func

--- app.go
+++ app.go
@@ -11,12 +11,19 @@ func main() {
 	port := os.Getenv("PORT")
 	log.Println("Hello World")
 	route := gin.Default()
+	run(route, port)
+}
+
+func initRouter(route *gin.Engine) {
 	route.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+}
 
+func run(route *gin.Engine, port string) {
+	initRouter(route)
 	err := route.Run(fmt.Sprintf(":%s", port))
 	if err != nil {
 		log.Fatalf("Server is not running %s", err)
